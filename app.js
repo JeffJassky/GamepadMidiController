f5eee94ab467a4cd3baec7b23c913fd1479ed40d
Jeff Jassky <jeff@jeffjassky.com>
2016-11-12 02:14:16 -0500

    Added basic command line interactions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // CONSTANTS
 const instruction = 144;
-const velocity = 127;
+const velocity = 0;
 
 // DEPENDENCIES
 var midi = require('midi');
@@ -9,7 +9,7 @@ var MIDIFile = require('midifile');
 var GamePad = require('node-gamepad');
 var fs = require('fs');
 var player = require('play-sound')(opts = {})
-
+var stdin = process.openStdin();
 
 
 // INIT MIDI
@@ -63,9 +63,52 @@ controller.on('r:press', function() {
 	console.log('pressed r');
 });
 controller.on('start:press', function() {
-	player.play('Jotun Demo.mp3', function(err){});
-    midiPlayer.play(function() {
-        console.log('Play ended');
-    });
+	setTimeout(function(){
+		player.play('Jotun Demo.mp3', function(err){});
+	}, 1000);
+
+	midiPlayer.play(function() {
+		console.log('Play ended');
+	});
+
 	console.log('pressed start');
+});
+
+
+var cliInstruction = instruction;
+var cliNote = 36;
+var cliVelocity = 50;
+
+var cliController = {
+	setnote: function(noteNumber){
+		cliNote = noteNumber;
+		this.trigger();
+	},
+	setvelocity: function(velocity){
+		cliVelocity = velocity;
+		this.trigger();
+	},
+	setinstruction: function(instruction){
+		cliInstruction = instruction;
+		this.trigger();
+	},
+	send: function(i,n,v){
+		console.log(i,n,v);
+		output.sendMessage([n,v,i]);
+	},
+	trigger: function(){
+		output.sendMessage([cliInstruction,cliNote,cliVelocity]);
+	}
+};
+
+stdin.addListener("data", function(d) {
+	var input = d.toString().trim();
+	if(input.length){
+		var split = input.split(' ');
+		var command = split[0];
+		args = split.slice(1);
+		if(cliController.hasOwnProperty(command)){
+			cliController[command].apply(cliController,args);
+		}
+	}
 });
